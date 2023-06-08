3464aac55161523ca81ae3bc5461d0ba8c055dc3
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-06-08 16:43:28 +0400

    Add Task 2, Task 3

---
+++ CSharp_Tasks/Task_2_SearchInArray/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How Many Numbers Want to enter :");
+            int enterNumb = Convert.ToInt32(Console.ReadLine());
+
+            float[] numbs = new float[enterNumb];
+
+            for(int i=0; i<enterNumb; i++)
+            {
+                Console.Write("Numbers Enterd - ", i);
+                numbs[i] = Convert.ToSingle(Console.ReadLine());
+            }
+
+            Console.Write("How Many Numbers Want to enter :");
+            var number = Convert.ToSingle(Console.ReadLine());
+
+            while(Console.ReadLine() != "End")
+            {
+                for (int i =0; i <= enterNumb; i++)
+                {
+                    if (numbs[i] == null)
+                    {
+                        Console.WriteLine("There is ", number);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharp_Tasks/Task_3_EvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Number {0}= ", i + 1);
+                
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 10; i++)
+                if (numbers[i] % 2 == 0)
+                    Console.Write("Even numbers : ", numbers[i]);
+        }
+    }
+}
