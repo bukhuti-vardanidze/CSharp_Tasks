1b124d000cadf5200063ca923e7c9de3b33731ca
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-06-08 16:06:47 +0400

    Add Task 1

---
+++ CSharp_Tasks/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbs = new int[5];
+            
+            for( int i=0; i<5; i++)
+            {
+                Console.Write("Number = ", i+1);
+                numbs[i] = Convert.ToInt32(Console.ReadLine());
+
+            } 
+
+            for(int j=4; j>=0; j--)
+            {
+                Console.WriteLine("Reverse Number :" + numbs[j]);
+            }
+
+            
+
+        }
+    }
+}
